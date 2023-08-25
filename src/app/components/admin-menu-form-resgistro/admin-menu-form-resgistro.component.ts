cc9b7b48063b21120fc5dc59f62cbb6e4329a81a
Felipe Vargas <fv79263@gmail.com>
2023-08-25 16:24:54 -0500

    Desarrollo lógica Front-End ahora el formulario de registro de productos tiene validaciones para cada uno de sus campos en caso de que el formulario este bien da una respuesta al usuarios con una alerta informándole que el producto fue agregado correctamente y lo redirije a la vista inicial de la web en caso de que el formulario este mal le informa al usuario atravesé de alertas

--- src/app/components/admin-menu-form-resgistro/admin-menu-form-resgistro.component.ts
+++ src/app/components/admin-menu-form-resgistro/admin-menu-form-resgistro.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -7,4 +9,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-menu-form-resgistro.component.css']
 })
 export class AdminMenuFormResgistroComponent {
+
+  formValues = {
+    URL: '',
+    categoria: '',
+    nombre: '',
+    precio: '',
+    descripcion: ''
+  }
+
+  constructor(private router: Router) { }
+
+  submitForm(): void {
+    if (this.formValues.URL === '' || this.formValues.categoria === '' || this.formValues.nombre === '' || this.formValues.precio === '' || this.formValues.descripcion === '') {
+      this.ShowErrorMessage('Todos los campos son obligatorios');
+
+    } else if (!this.formValues.URL.includes('https://') && !this.formValues.URL.includes('http://')) {
+      this.ShowErrorMessage('La URL debe tener un formato válido');
+
+    } else if (this.formValues.nombre.length > 50) {
+      this.ShowErrorMessage('El nombre no puede tener más de 20 caracteres');
+
+    }else if (!/^\d+$/.test(this.formValues.precio)) {
+      this.ShowErrorMessage('El precio debe tener solo números');
+    }else if (this.formValues.descripcion.length > 200) {
+      this.ShowErrorMessage('La descripción no puede tener más de 200 caracteres');
+    }else {
+      this.formValues = {
+        URL: '',
+        categoria: '',
+        nombre: '',
+        precio: '',
+        descripcion: ''
+      }
+      this.showSuccessMessage('Producto registrado exitosamente');
+
+      this.router.navigate(['/'])
+
+    }
+  }
+
+  private showSuccessMessage(message: string): void {
+    Swal.fire({
+      icon: 'success',
+      title: '¡Éxito!',
+      text: message
+    })
+  }
+
+  private ShowErrorMessage(message: string): void {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: message
+    })
+  }
 }
